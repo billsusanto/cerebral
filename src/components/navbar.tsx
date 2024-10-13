249426639d6c57426945c439b3d46d383f337a17
billsusanto <billsusanto01@gmail.com>
2024-10-13 06:38:43 -0700

    Now ready for deployment

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,22 @@
+import { DatabaseIcon } from "lucide-react";
 import Link from "next/link";
 
 export function Navbar() {
   return (
-    <div className="flex w-full items-center justify-between pt-6 sm:pt-8 lg:pt-10 pb-2 sm:pb-2 lg:pb-4 px-4 sm:px-8 md:px-16 lg:px-32">
-      <div className="flex items-center gap-x-16">
-        <Link href="/" className="text-2xl md:text-3xl lg:text-4xl font-bold text-[#f6f930]">Ind<span className="text-[#ffffff]">order</span>.</Link>
-        <Link href="/dashboard" className="text-2xl md:text-3xl lg:text-4xl  text-[#f6f930] font-semibold">Dashboard</Link>
-      </div>
+    <div className="flex w-full items-center justify-between px-4 pb-2 pt-6 sm:px-8 sm:pb-2 sm:pt-8 md:px-16 lg:px-32 lg:pb-4 lg:pt-10">
+      <Link
+        href="/"
+        className="text-2xl font-bold text-[#f6f930] md:text-3xl lg:text-4xl"
+      >
+        Ind<span className="text-[#ffffff]">order</span>.
+      </Link>
+
+      <Link
+        href="/dashboard"
+        className="text-sm font-semibold text-[#f6f930] md:text-xl lg:text-xl hover:brightness-125 transition-all"
+      >
+        <DatabaseIcon size={40} />
+      </Link>
     </div>
   );
 }
